2113352d4b943853d5e052c694df112ceb23693f
FurrySparkle <97181304+FurrySparkle@users.noreply.github.com>
2024-12-22 09:48:26 -0700

    further config for vercel

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ChatPage from '../../src/components/pages/chat';
-export const runtime = 'edge';
+//export const runtime = 'edge';
 export default function NewChatPage() {
   return <ChatPage  />;
 }
--- app/s/[id]/page.tsx
+++ app/s/[id]/page.tsx
@@ -5,7 +5,7 @@
 
 import React from 'react';
 import ChatPage from '../../src/components/pages/chat';
-export const runtime = 'edge';
+//export const runtime = 'edge';
 
 
 export default function SharedChatPage() {
