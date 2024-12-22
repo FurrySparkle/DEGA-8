9857a4685c729b09e0504aabf2019e08f013815c
FurrySparkle <97181304+FurrySparkle@users.noreply.github.com>
2024-12-22 09:09:31 -0700

    Added edge runtime
    please just work

--- app/s/[id]/page.tsx
+++ app/s/[id]/page.tsx
@@ -1,8 +1,12 @@
 // filepath: /C:/Users/15054/Documents/GitHub/DEGA-8/app/s/[id]/page.tsx
+
+
 'use client';
 
 import React from 'react';
 import ChatPage from '../../src/components/pages/chat';
+export const runtime = 'edge';
+
 
 export default function SharedChatPage() {
   return <ChatPage share={true} />;
