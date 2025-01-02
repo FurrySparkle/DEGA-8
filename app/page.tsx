92b0a1b82e7cf37a4e670d3c45f2bb09cd2cefae
FurrySparkle <97181304+FurrySparkle@users.noreply.github.com>
2025-01-02 16:55:33 -0700

    Quick build p[ush

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import React from 'react';
-import { usePathname, useParams} from 'next/navigation';
+import { usePathname} from 'next/navigation';
 
 import dynamic from 'next/dynamic';
 import './src/index.scss';
 
-const ChatPage = dynamic(() => import('./src/components/pages/chat'));
-const LandingPage = dynamic(() => import('./src/components/pages/landing'));
+
+const LandingPage = dynamic(() => import('./src/components/pages/landing.tsx'));
 
 function Page() {
   
-  const param =  useParams<{ id: string }>();
+  
   // Determine which component to render based on the current route
   const path = usePathname();
   let content;
@@ -19,14 +19,7 @@ function Page() {
   if (path === '/') {
     content = <LandingPage landing={true} />;
   } 
-  // else if (path.startsWith('/chat/')) {
-  //   console.log('ChatPage||', param!.id);
-  // content = <ChatPage id={param!.id} />;
-  // } else if (path.startsWith('/s/')) {
-  //   content = <ChatPage share={true} />;
-  // } else {
-  //   content = <div>Page not found</div>;
-  // }
+  
 
   return (
     <>
